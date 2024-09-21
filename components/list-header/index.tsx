7b85ab550dd92ebc613c89f88458c6dd725b708e
FabioKita <kitayomofabioyuichi@gmail.com>
2024-09-21 11:24:21 -0400

    added header transition

--- components/list-header/index.tsx
+++ components/list-header/index.tsx
@@ -2,7 +2,7 @@ import { getHeaderTitle } from '@react-navigation/elements';
 import { NativeStackHeaderProps } from '@react-navigation/native-stack';
 import { Alert, BackHandler, StyleSheet, Text, View } from 'react-native';
 import { router, useFocusEffect } from 'expo-router';
-import { useCallback, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 
 import AddIcon from './add-icon';
 import CancelIcon from './cancel-icon';
@@ -12,6 +12,7 @@ import HeaderButton from '@/components/inputs/header-button';
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { removeAllNotes } from '@/api/note-api';
 import LoadingModal from '../loading-modal';
+import FadeView from '../containers/fade-div';
 
 interface ListHeaderProps extends NativeStackHeaderProps{
 	selectedEntries:number[],
@@ -30,6 +31,42 @@ export default function ListHeader({
 	const selectedCount = selectedEntries.length;
 	const isSelectMode = selectedCount > 0;
 
+    return <View style={styles.container}>
+		{/*Render this if not select mode*/}
+		<FadeView style={styles.content}  visible={!isSelectMode}>
+			<Text style={styles.title}>{title}</Text>
+			<View style={styles.buttonContainer}>
+				<HeaderButton onPress={()=>router.navigate("/add-note")}>
+					<AddIcon/>
+				</HeaderButton>
+			</View>
+		</FadeView>
+
+		{/*Otherwise This*/}
+		<SelectModeHeaderContent 
+			selectedEntries={selectedEntries} 
+			setSelectedEntries={setSelectedEntries}
+		/>
+	</View>
+}
+
+interface SelectModeHeaderContentProps{
+	selectedEntries:number[],
+	setSelectedEntries:(v:number[])=>void
+}
+
+function SelectModeHeaderContent({
+	selectedEntries,
+	setSelectedEntries
+}:SelectModeHeaderContentProps){
+	const selectedCount = selectedEntries.length;
+	const isSelectMode = selectedCount > 0;
+
+	const [displaySelectedCount, setDisplaySelectedCount] = useState(selectedCount);
+	useEffect(()=>{
+		if(isSelectMode) setDisplaySelectedCount(selectedCount)
+	},[selectedEntries])
+
 	const queryClient = useQueryClient();
 	const {mutateAsync, isPending} = useMutation({
 		mutationFn:removeAllNotes,
@@ -37,7 +74,7 @@ export default function ListHeader({
 			queryClient.invalidateQueries({queryKey:["notes"]});
 			setSelectedEntries([]);
 		}
-	})
+	});
 
 	useFocusEffect(
 		useCallback(() => {
@@ -67,41 +104,61 @@ export default function ListHeader({
 		)
 	}
 
-	const renderSelectMode = ()=>{
-		return <View style={styles.selectModeContainer}>
+	return <>
+		{isPending&&<LoadingModal/>}
+		<FadeView style={styles.selectContent} visible={isSelectMode}>
 			<View style={styles.buttonContainer}>
 				<HeaderButton onPress={()=>setSelectedEntries([])}>
 					<CancelIcon/>
 				</HeaderButton>
-				<Text style={styles.title}>{selectedCount}</Text>
+				<Text style={styles.title}>{displaySelectedCount}</Text>
 			</View>
 			<View style={styles.buttonContainer}>
-				<HeaderButton onPress={handleAlert}>
+				<HeaderButton onPress={()=>isSelectMode&&handleAlert()}>
 					<TrashIcon/>
 				</HeaderButton>
 			</View>
-		</View>
-	}
-
-	const render = ()=>{
-		return <>
-			<View style={styles.container}>
-				<Text style={styles.title}>{title}</Text>
-				<View style={styles.buttonContainer}>
-					<HeaderButton onPress={()=>router.navigate("/add-note")}>
-						<AddIcon/>
-					</HeaderButton>
-				</View>
-			</View>
-		</>
-	}
-
-    return <View>
-		{isPending&&<LoadingModal/>}
-		{isSelectMode?renderSelectMode():render()}
-	</View>
+		</FadeView>
+	</>
 }
 
+const styles = StyleSheet.create({
+	container:{
+		marginTop:32,
+		height:72,
+		backgroundColor:"#FFFFFF",
+		borderBottomWidth:1,
+		borderBottomColor:"#E5E7EB",
+	},
+	title:{
+		fontSize:20,
+		fontWeight:"bold",
+	},
+	buttonContainer:{
+		flexDirection:"row",
+		alignItems:"center",
+		gap:8,
+	},
+	content:{
+		position:"absolute",
+		top:0, bottom:0, left:0, right:0,
+		flexDirection:"row",
+		alignItems:"center",
+		justifyContent:"space-between",
+		paddingHorizontal:20
+	},
+	selectContent:{
+		position:"absolute",
+		top:0, bottom:0, left:0, right:0,
+		flexDirection:"row",
+		alignItems:"center",
+		justifyContent:"space-between",
+		paddingHorizontal:20,
+		paddingLeft:0
+	},
+});
+
+/*
 const styles = StyleSheet.create({
 	container:{
 		marginTop:32,
@@ -153,3 +210,4 @@ const styles = StyleSheet.create({
 		fontWeight:"bold"
 	}
 })
+*/
